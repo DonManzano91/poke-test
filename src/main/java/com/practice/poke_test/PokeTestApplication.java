d04e128b5f52904d6f709b4bcc1a833807650263
DonManzano91 <alex.batt99@gmail.com>
2025-06-12 17:36:09 -0600

    First Commit, Project Structure

---
+++ src/main/java/com/practice/poke_test/PokeTestApplication.java
@@ -0,0 +1,13 @@
+package com.practice.poke_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/poke_test/PokeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.poke_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
